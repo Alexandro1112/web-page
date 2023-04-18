721d8a2d2e8aa665883266147c49b80cbfeede7b
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2023-04-18 22:05:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function gitonclick() {
+    window.open('https://github.com/Alexandro1112')
+}
+
+
+function youtubeclick(){
+    window.open('https://www.youtube.com/channel/UCSGPqm15jFz-6vP8Zh5s7kQ')
+
+}
+
+function vkclick(){
+    window.open('https://vk.com/id583352745')
+}
+
+function close_page(){
+    window.close()
+
+}
+
+ 
